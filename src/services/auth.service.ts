af89831e52fcca87aa1fbef174d7b9f28b456d89
mjloperap20 <mlopera@sura.com.co>
2021-11-15 20:41:12 -0500

    commit servicio de login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,9 @@
 import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {config} from '../config/config';
+import {Usuario} from '../models';
+import {UsuarioRepository} from '../repositories';
+const jwt = require('jsonwebtoken');
 
 // Nuevas librerias
 const generator = require("password-generator");
@@ -6,7 +11,8 @@ const cryptoJS = require("crypto-js");
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AuthService {
-  constructor(/* Add @inject to inject parameters */) { }
+  constructor(@repository(UsuarioRepository)
+  public usuarioRepository: UsuarioRepository) { }
 
   //Generacion de claves
   GenerarClave() {
@@ -14,9 +20,47 @@ export class AuthService {
     return clave;
   }
 
+  //Cifrado de la clave anteriormente generada
   CifrarClave(clave: String) {
     let claveCifrada = cryptoJS.MD5(clave).toString();
     return claveCifrada;
   }
+
+  //Generación del token JWT
+  GenerarTokenJWT(usuario: Usuario) {
+    let token = jwt.sign({
+      data: {
+        id: usuario.id,
+        correo: usuario.correo,
+        nombre: usuario.nombre + " " + usuario.apellidos
+      }
+    }, config.claveJWT)
+
+    return token
+  }
+
+  //Validación del token
+  validarTokenJWT(token: string) {
+    try {
+      let datos = jwt.verify(token, config.claveJWT);
+      return datos;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  //Autenticacion, validamos que la constraseña y usuario ingresado si esten registrados
+  IdentificarPersona(correo: string, password: string) {
+    try {
+      let p = this.usuarioRepository.findOne({where: {correo: correo, password: password}})
+      if (p) {
+        return p;
+      }
+      return false;
+    } catch {
+      return false;
+    }
+  }
+
 
 }
